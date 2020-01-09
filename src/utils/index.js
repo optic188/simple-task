76b34bb74fb670fc0838f0dac6adbaefaa1914a6
Alexander Sazonov <optic18@gmail.com>
2020-01-09 15:23:26 +0200

    remove unecessary utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,1 @@
-export const serchUtil = (itemsArr,searchStringArg) => {
-    let searchString = searchStringArg.toLowerCase()
-    console.log(searchString)
-    let resArr = []
-    for (let i=0; i<itemsArr.length; i++) {
-        if(itemsArr[i].name.toLowerCase().includes(searchString)
-        || itemsArr[i].role.toLowerCase().includes(searchString)
-        || itemsArr[i].connected.toLowerCase().includes(searchString)
-        ) {
-            resArr.push(itemsArr[i])
-        }
-    }
-    return resArr
-}
+
